fd0af5cfcc54c82784320830fe4dbeba975e8da3
dse12345z <dse12345z@gmail.com>
2014-11-10 17:03:47 +0800

    Initial Commit

---
+++ AVAudioPlayer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AVAudioPlayer
+//
+//  Created by daisuke on 2014/11/10.
+//  Copyright (c) 2014年 Daisuke. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
